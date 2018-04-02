1daf9d344866886f82ec9746d6fecbbc5fc106a3
liufushihai <liufushihai@163.com>
2018-04-02 18:38:24 +0800

    add splash activity

---
+++ app/src/main/java/pers/liufushihai/panocamclient/activity/SplashActivity.java
@@ -0,0 +1,46 @@
+package pers.liufushihai.panocamclient.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.RelativeLayout;
+
+import pers.liufushihai.panocamclient.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        RelativeLayout layoutSplash = findViewById(R.id.activity_splash);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0.1f,1.0f);
+        alphaAnimation.setDuration(1000);   //设置动画播放时长为1000毫秒(1秒)
+        layoutSplash.startAnimation(alphaAnimation);
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+//                页面的跳转
+                Intent intent = new Intent(
+                        SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
